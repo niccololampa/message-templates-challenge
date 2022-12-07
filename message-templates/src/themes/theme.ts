be483b663f80fa461a8e03936d524b628b542306
niccololampa <niccololampa@gmail.com>
2022-12-07 20:03:26 +0800

    Use CircularStd font provided

--- message-templates/src/themes/theme.ts
+++ message-templates/src/themes/theme.ts
@@ -1,6 +1,23 @@
 import { createTheme } from "@mui/material/styles"
 
 export const appTheme = createTheme({
+  typography: {
+    fontFamily: "CircularStd, Arial",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'CircularStd';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('CircularStd'), local('CircularStd-Black'), url("../fonts/CircularStd-Black.ttf") format('ttf');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
   navigationBar: {
     logo: {
       width: "186px",
